55b7cf602dcbca828b22a880ea7155546bf13bed
yoonustehrani <yoonustehrani28@gmail.com>
2024-12-20 14:37:57 +0330

    basic routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import {roboto} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${roboto.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const roboto = Roboto({ subsets: ['latin'], weight: '500' });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
